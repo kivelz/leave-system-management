b2f7e7597720ba5a6e640e977763e67c813175f0
antaresQ <antaresQ@DESKTOP-7J4MING>
2019-05-30 14:04:49 +0800

    Revert "Removed unused leavedetails model and related instantiations"
    This reverts commit 55dd4bac15d1015955c299c104d698c8546c0385.

---
+++ src/main/java/com/leave/system/model/LeaveRecords.java
@@ -0,0 +1,18 @@
+package com.leave.system.model;
+
+import java.util.ArrayList;
+
+public class LeaveRecords {
+
+	private ArrayList<Leavedetail> leavelist;
+
+	public ArrayList<Leavedetail> getLeavelist() {
+		return leavelist;
+	}
+
+	public void setLeavelist(ArrayList<Leavedetail> leavelist) {
+		this.leavelist = leavelist;
+	}
+	
+	
+}
